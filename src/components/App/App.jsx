6f0b9093965bd098aeb51504c2277d0ee24da78f
Dark-ManX <sergiosis@ukr.net>
2022-07-23 17:28:16 +0300

    last fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,9 +7,9 @@ import Filter from "../Filter/Filter";
 
 export default function App() {
 
-  console.log(JSON.parse);
+  const localStorageContacts = JSON.parse(localStorage.getItem('contacts')) 
 
-  const [contacts, setContacts] = useState(JSON.parse(localStorage.getItem('contacts')) ?? [
+  const [contacts, setContacts] = useState(localStorageContacts ?? [
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
